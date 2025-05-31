7922c4b70de05b225ca6b79b2b08d6bcf0892fde
Parham Zolfaghari <parhamzolfaqari@gmail.com>
2025-05-31 06:38:45 -0400

    control flow in name only

---
+++ ControlFlow/control_flow.js
@@ -0,0 +1,12 @@
+let myName = "parham";
+let age = 29;
+let nothing; 
+let nothing2 = null;
+let friends = ["bache", "boragh"];
+
+console.log("type of name: ", typeof myName);
+console.log("type of age: ", typeof age);
+console.log("typeof nothing: ", typeof nothing);
+console.log("type of nothing2: ", typeof nothing2);
+console.log("type of friends: ", typeof friends);
+console.log("typeof typeof: ", typeof typeof age);
