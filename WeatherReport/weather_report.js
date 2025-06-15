9d0d0cbac5a5e76bda5e48fd011ef8193b9a90ec
parham <parhamzolfaqari@gmail.com>
2025-06-15 11:53:46 -0400

    fetch

--- WeatherReport/weather_report.js
+++ WeatherReport/weather_report.js
@@ -1,9 +1,24 @@
 function showWeatherDetails(event) {
+    debugger;
     event.preventDefault();
 
     const lat = document.getElementById("cityLat").value;
     const lon = document.getElementById("cityLon").value;
     const apiKey = "1e907919d4584f1eedb826d1f1f4da80";
-    const part = "minutely";
-    const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={apiKey}`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById("weatherInfo");
+            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+            <p>Temperature: ${data.main.temp} &#8451;</p>
+            <p>Weather: ${data.weather[0].description}</p>`;
+        })
+        .catch(error => {
+            const weatherInfo = document.getElementById("weatherInfo");
+            weatherInfo.innerHTML = `<p>failed to get weather: ${error}</p>`;
+        });
 }
+
+document.getElementById("weatherForm").addEventListener("submit", showWeatherDetails);
